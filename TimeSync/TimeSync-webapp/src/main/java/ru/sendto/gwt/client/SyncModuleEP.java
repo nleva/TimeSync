4aadb4572798d08ff669eff7709c6fc94e9efad6
nleva <n.lev.a@ya.ru>
2017-09-13 09:46:03 +0300

    urls -> /tsync

--- TimeSync/TimeSync-webapp/src/main/java/ru/sendto/gwt/client/SyncModuleEP.java
+++ TimeSync/TimeSync-webapp/src/main/java/ru/sendto/gwt/client/SyncModuleEP.java
@@ -5,11 +5,11 @@ import com.google.gwt.core.client.GWT;
 
 import ru.sendto.rest.gwt.Websocket;
 
-public class ModuleEP implements EntryPoint {
+public class SyncModuleEP implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		Websocket.init(GWT.getHostPageBaseURL() + "ws");
+		Websocket.init(GWT.getHostPageBaseURL() + "tsync");
 		Sync.init();
 	}
 
