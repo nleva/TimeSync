171bf1fdb54c08d6524eabe8d49d5aa776af91f4
nleva <n.lev.a@ya.ru>
2017-09-22 19:00:40 +0300

    update websoket gwt

--- TimeSync/TimeSync-webapp/src/main/java/ru/sendto/gwt/client/Sync.java
+++ TimeSync/TimeSync-webapp/src/main/java/ru/sendto/gwt/client/Sync.java
@@ -1,5 +1,6 @@
 package ru.sendto.gwt.client;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Set;
 import java.util.TreeSet;
@@ -22,38 +23,67 @@ public class Sync {
 	}-*/;
 
 	public static void init() {
-		Scheduler.get().scheduleFixedPeriod(Sync::sendRequest, 1000);
+		Scheduler.get().scheduleFixedPeriod(Sync::sendRequest, 300);
 		sendRequest();
 	}
 
-	static int max = 10;
+	static int max = 20;
 
 	private static boolean sendRequest() {
 		Websocket.send(new TimeSyncDto().setClient(new Date().getTime()));
 		return max-- > 0;
 	}
 
-	static Set<Integer> delta = new TreeSet<>();
+	static class TimeEntry{
+		public TimeEntry(int duration, int shift) {
+			super();
+			this.duration = duration;
+			this.shift = shift;
+		}
+		int duration;
+		int shift;
+	}
+	static Set<Integer> delta = new TreeSet<>((i,j)->i>j?1:-1);
+	static Set<TimeEntry> set = new TreeSet<>((i,j)->i.duration>j.duration?1:-1);
 
 	static {Bus.get().listen(TimeSyncDto.class, Sync::setSync);}
 	static void setSync(TimeSyncDto dto) {
 		long clientTs = new Date().getTime();
 		long requestDuration = clientTs - dto.getClient();
 		long serverTimeShift =  dto.getServer() + requestDuration / 2 - clientTs; 
-		// serverTime = ClientTime-duration/2 + serverTimeShift
-		// clientTime = serverTime 
-		delta.add(((int) serverTimeShift));
-		Object[] results = delta.toArray();
-		Integer c = ((Integer) results[results.length / 2]);
-		setServerTimeShift(c);
+
+		final TimeEntry e = new TimeEntry((int)(clientTs-dto.getClient()), (int)serverTimeShift);
+		set.add(e);
+		
+		if(set.size()>=3) {
+			final ArrayList<TimeEntry> list = new ArrayList<>(set);
+			int fistResultsCount = (int) (set.size()*0.7);
+			final TreeSet<TimeEntry> filtred = new TreeSet<>((i,j)->i.shift>j.shift?1:-1);
+			for(int i=0; i<fistResultsCount; i++) {
+				list.get(i);
+				filtred.add(list.get(i));
+			}
+			TimeEntry mid=(TimeEntry) filtred.toArray()[(fistResultsCount+1)/2];
+			setServerTimeShift(mid.shift);
+			setServerPing(mid.duration);
+			
+		}
 	}
 
 	public static native void setServerTimeShift(int shift)/*-{
 		$wnd.serverTimeShift = shift;
 	}-*/;
 
-	public static native int getserverTimeShift()/*-{
+	public static native int getServerTimeShift()/*-{
 		return $wnd.serverTimeShift;
+	}-*/;
+
+	public static native void setServerPing(int ping)/*-{
+		$wnd.serverPing = ping;
+	}-*/;
+
+	public static native int getServerPing()/*-{
+		return $wnd.serverPing;
 	}-*/;
 
 }
